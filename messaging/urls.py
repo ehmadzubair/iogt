10f8e1e92c0360fbe3a935eddb4c62d26ca4a7e2
Ehmad Zubair <ezubair@gmail.com>
2021-07-07 18:14:33 +0500

    Added Blank pages

--- messaging/urls.py
+++ messaging/urls.py
@@ -7,6 +7,9 @@ app_name = "messaging"
 
 urlpatterns = [
     path('api/', include(api_urls)),
+    path('v2/inbox', views.InboxIndexView.as_view(), name='inboxv2'),
+    path('v2/message', views.MessageDetailView.as_view(), name='messagev2'),
+
     path('inbox/', views.InboxView.as_view(), name="inbox"),
     path('create/', views.MessageCreateView.as_view(), name="message_create"),
     path('thread/<int:pk>/', views.ThreadView.as_view(), name="thread_view"),
